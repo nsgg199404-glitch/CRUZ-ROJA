bcb03a9493242e28636e51469acbac603b15e20f
Norma Garcia <nsgg199404@gmail.com>
2026-09-20 15:42:57 -0600

    Agrega registro de combustible con foto del ticket

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // <-- Agregamos Firestore
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,5 +17,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app); // Inicializamos la base de datos
+const storage = getStorage(app);
 
-export { app, auth, db }; // Exportamos 'db' para poder usarla
+export { app, auth, db, storage }; // Exportamos 'db' para poder usarla
